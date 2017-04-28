86c450adb4504a2ecdab183b01cce3ba19c24dea
Eric M Whitmire <emwhit@cs.washington.edu>
2017-04-27 20:02:18 -0700

    appveyor

--- setup.py
+++ setup.py
@@ -47,6 +47,9 @@ setup(
     license='LICENSE.txt',
     description='Pomegranate is a graphical models library for Python, implemented in Cython for speed.',
     ext_modules=extensions,
+    setup_requires=[
+        "numpy >= 1.8.0",
+    ],
     install_requires=[
         "cython >= 0.22.1",
         "numpy >= 1.8.0",
